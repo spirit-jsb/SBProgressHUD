88a9b0c5c8956b63b95bf4b65860c9e3ed36a4d6
Max <sibo_jian_29903549@163.com>
2023-07-05 21:12:49 +0800

    Initial SBProgressHUD project

---
+++ Demo/Demo/Spirit-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SBProgressHUD-Demo
+//
+//  Created by Max on 2023/7/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sources/SBProgressHUD.h
@@ -0,0 +1,18 @@
+//
+//  SBProgressHUD.h
+//  SBProgressHUD
+//
+//  Created by Max on 2023/7/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SBProgressHUD.
+FOUNDATION_EXPORT double SBProgressHUDVersionNumber;
+
+//! Project version string for SBProgressHUD.
+FOUNDATION_EXPORT const unsigned char SBProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBProgressHUD/PublicHeader.h>
+
+
